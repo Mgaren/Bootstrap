ae43c67f46f22caddee07a7add6aaf72eabdb4ea
Mgaren <basketeur009@gmail.com>
2020-10-21 13:20:04 +0200

    quete bootstrap

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title> Quete bootstrap </title>
+</head>
+<body>
+    <div class="container">
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-9" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-3" id="orange"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1 offset-3" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-2" id="orange"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1 offset-2" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-1" id="orange"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1 offset-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-1" id="orange"></div>
+            <div class="col-1" id="yellow"></div>
+            <div class="col-1" id="orange"></div>
+            <div class="col-1 offset-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1 offset-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1 offset-3" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1 offset-2" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1 offset-3" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1 offset-4" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-1 offset-4" id="black"></div>
+            <div class="col-1" id="black"></div>
+            <div class="col-1" id="black"></div>
+        </div>
+    </div>    
+</body>
+</html>
